d7f3891913a2a311d9578eeb968b871e7ff24580
jusylopes <jusylopes@outlook.com>
2024-10-07 23:04:40 -0300

    feat: add AssetsManager class for managing assets paths

---
+++ lib/utils/assets_manager.dart
@@ -0,0 +1,7 @@
+class AssetsManager {
+  static String fontFamilyRegular = 'assets/fonts/Roboto-Regular.ttf';
+  static String fontFamilyBold = 'assets/fonts/Roboto-Bold.ttf';
+
+  static String imagePath = 'assets/images';
+  static String imageLogo = '$imagePath/logo.png';
+}
