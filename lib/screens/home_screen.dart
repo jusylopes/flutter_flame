5a9cc61c9cd7b48ff70d748587bbc5deeb4a6144
jusylopes <jusylopes@outlook.com>
2024-10-13 23:36:15 -0300

    feat:  implement Bluetooth connection in HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,62 @@
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  BluetoothConnection? connection;
+  String dataReceived = 'Sem dados';
+
+  @override
+  void initState() {
+    super.initState();
+    connectToBluetooth();
+  }
+
+  void connectToBluetooth() async {
+    try {
+      connection = await BluetoothConnection.toAddress('21:13:ED9A');
+      print('Conectado ao dispositivo Bluetooth');
+
+      connection!.input!.listen((Uint8List data) {
+        setState(() {
+          dataReceived = String.fromCharCodes(data).trim();
+        });
+        print('Recebido: $dataReceived');
+        
+      }).onDone(() {
+        print('Conexão encerrada.');
+      });
+    } catch (error) {
+      print('Erro ao conectar: $error');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text('Status: $dataReceived'),
+        ElevatedButton(
+          onPressed: () {
+            connectToBluetooth();
+          },
+          child: const Text('Conectar ao Bluetooth'),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    connection?.dispose();
+    super.dispose();
   }
 }
