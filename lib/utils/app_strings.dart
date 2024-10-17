deea53ad1aa1ddae245755c2628556e362f08732
jusylopes <jusylopes@outlook.com>
2024-10-17 00:20:45 -0300

    feat: add Bluetooth connection strings to AppStrings class

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -1,4 +1,13 @@
 class AppStrings {
   static const String appName = 'Flame';
   static const String appFontFamily = '';
+
+  static const String bluetoothConnectionAddress = '';
+  static const String connectingMessage = 'Conectando...';
+  static const String timeoutError = 'Tempo de conexão esgotado';
+  static const String pairingSuccess = 'Pareamento OK!';
+  static const String receivedDataError = 'Erro ao receber dados: ';
+  static const String connectionFailure = 'Falha ao conectar ou parear';
+  static const String permissionsNotGranted =
+      'Permissões necessárias não foram concedidas.';
 }
