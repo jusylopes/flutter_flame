ed86fe11c81e0f8c8ad85ebee7c845db5f01df19
jusylopes <jusylopes@outlook.com>
2024-10-22 14:58:02 -0300

    refactor: Update assets_manager.dart to add new image paths

--- lib/utils/assets_manager.dart
+++ lib/utils/assets_manager.dart
@@ -4,4 +4,10 @@ class AssetsManager {
 
   static String imagePath = 'assets/images';
   static String imageLogo = '$imagePath/logo.png';
+  
+  static String imageCloud = '$imagePath/cloud.png';
+  static String imageEyesFlame = '$imagePath/eyes_flame.png';
+  static String imageEyes = '$imagePath/eyes.png';
+  static String imageFlameGif = '$imagePath/flame.gif';
+  static String imageTreeGif = '$imagePath/tree.gif';
 }
