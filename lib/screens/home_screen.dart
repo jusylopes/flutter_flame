257f9f6c103c9f132cdcf69f88be9a94e245e14c
jusylopes <jusylopes@outlook.com>
2024-11-06 00:51:43 -0300

    feat: add phone call functionality and update permissions in HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,97 +2,114 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_flame/blocs/bluetooth/bluetooth_cubit.dart';
 import 'package:flutter_flame/blocs/bluetooth/bluetooth_state.dart';
-import 'package:flutter_flame/utils/app_colors.dart';
 import 'package:flutter_flame/utils/app_strings.dart';
 import 'package:flutter_flame/utils/assets_manager.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
     final bluetoothCubit = context.read<BluetoothCubit>();
     bluetoothCubit.requestPermissions().then((_) {
       bluetoothCubit.connectToDevice(AppStrings.bluetoothConnectionAddress);
     });
+  }
 
+  Future<void> _makePhoneCall({required String phoneNumber}) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri, mode: LaunchMode.externalApplication);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(AssetsManager.imageCloud),
-          BlocBuilder<BluetoothCubit, BluetoothState>(
-            builder: (context, state) {
-              final bool isFireDetected = state.receivedData == "1";
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset(AssetsManager.imageCloud),
+            BlocBuilder<BluetoothCubit, BluetoothState>(
+              builder: (context, state) {
+                final bool isFireDetected = state.receivedData == "1";
 
-              if (state.isConnecting) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                if (state.isConnecting) {
+                  return Column(
                     children: [
                       const CircularProgressIndicator(),
                       const SizedBox(height: 16),
                       Text(state.statusMessage),
                     ],
-                  ),
-                );
-              }
+                  );
+                }
 
-              return Stack(
-                alignment: Alignment.center,
-                children: [
-                  Image.asset(AssetsManager.imageTreeGif),
-                  Image.asset(
-                    isFireDetected
-                        ? AssetsManager.imageEyesFlame
-                        : AssetsManager.imageEyes,
-                  ),
-                  if (isFireDetected) Image.asset(AssetsManager.imageFlameGif),
-                  Column(
-                    children: [
-                      const SizedBox(
-                        height: 160,
-                      ),
-                      SizedBox(
-                        width: 260,
-                        child: Column(
-                          children: [
-                            Text(
-                              isFireDetected
-                                  ? "fogo detectado"
-                                  : "tudo certo por aqui!",
-                              style: const TextStyle(
-                                fontSize: 60,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: AppStrings.appFontFamily,
-                                color: Colors.black,
-                                height: 0.75,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 8,
-                            ),
-                            if (isFireDetected)
-                              RichText(
-                                text: const TextSpan(
-                                  text: 'ligar para 193, urgente!',
-                                  style: TextStyle(
-                                    fontSize: 30,
-                                    backgroundColor: AppColors.flameColor,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(AssetsManager.imageTreeGif),
+                    Image.asset(
+                      isFireDetected
+                          ? AssetsManager.imageEyesFlame
+                          : AssetsManager.imageEyes,
+                    ),
+                    if (isFireDetected)
+                      Image.asset(AssetsManager.imageFlameGif),
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 160,
+                        ),
+                        SizedBox(
+                          width: 260,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                isFireDetected
+                                    ? "fogo detectado"
+                                    : "tudo certo por aqui!",
+                                style: const TextStyle(
+                                  fontSize: 55,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: AppStrings.appFontFamily,
+                                  color: Colors.black,
+                                  height: 0.9,
                                 ),
                               ),
-                          ],
+                              if (isFireDetected)
+                                GestureDetector(
+                                  onTap: () =>
+                                      _makePhoneCall(phoneNumber: '193'),
+                                  child: const Text(
+                                    "ligar para 193",
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontFamily: AppStrings.appFontFamily,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              );
-            },
-          ),
-        ],
+                      ],
+                    )
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
