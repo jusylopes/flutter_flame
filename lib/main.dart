dadb17ad4619e2eec7306cc968f339961f9ec13b
jusylopes <jusylopes@outlook.com>
2024-11-11 21:36:38 -0300

    feat: initialize notification and background services in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flame/flame_app.dart';
+import 'package:flutter_flame/services/background_service.dart';
+import 'package:flutter_flame/services/notification_service.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() => runApp(const FlameApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  NotificationService notificationService = NotificationServiceImpl();
+  notificationService.initialize();
+
+  final backgroundService = BackgroundServiceImpl();
+  backgroundService.connect(
+    task: "bluetoothMonitoringTask",
+    inputData: {'receivedData': '1'},
+  );
+
+  runApp(const FlameApp());
+}
