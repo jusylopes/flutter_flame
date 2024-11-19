1f658c92b5f379ed8a486b4a50ab11e78df5dd40
jusylopes <jusylopes@outlook.com>
2024-11-18 23:47:58 -0300

    feat: add FireScreen and integrate with HomeScreen navigation

---
+++ lib/screens/fire_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_flame/blocs/bluetooth/bluetooth_cubit.dart';
+import 'package:flutter_flame/blocs/bluetooth/bluetooth_state.dart';
+import 'package:flutter_flame/utils/app_strings.dart';
+import 'package:flutter_flame/utils/assets_manager.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class FireScreen extends StatefulWidget {
+  const FireScreen({super.key});
+
+  @override
+  State<FireScreen> createState() => _FireScreenState();
+}
+
+class _FireScreenState extends State<FireScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final bluetoothCubit = context.read<BluetoothCubit>();
+    bluetoothCubit.requestPermissions().then((_) {
+      bluetoothCubit.connectToDevice(AppStrings.bluetoothConnectionAddress);
+    });
+  }
+
+  Future<void> _makePhoneCall({required String phoneNumber}) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri, mode: LaunchMode.externalApplication);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset(AssetsManager.imageCloud),
+            BlocBuilder<BluetoothCubit, BluetoothState>(
+              builder: (context, state) {
+                final bool isFireDetected = state.receivedData == "1";
+
+                if (state.isConnecting) {
+                  return Column(
+                    children: [
+                      const CircularProgressIndicator(),
+                      const SizedBox(height: 16),
+                      Text(state.statusMessage),
+                    ],
+                  );
+                }
+
+                return Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Image.asset(AssetsManager.imageTreeGif),
+                    Image.asset(
+                      isFireDetected
+                          ? AssetsManager.imageEyesFlame
+                          : AssetsManager.imageEyes,
+                    ),
+                    if (isFireDetected)
+                      Image.asset(AssetsManager.imageFlameGif),
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 160,
+                        ),
+                        SizedBox(
+                          width: 260,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                isFireDetected
+                                    ? "fogo detectado"
+                                    : "tudo certo por aqui!",
+                                style: const TextStyle(
+                                  fontSize: 55,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: AppStrings.appFontFamily,
+                                  color: Colors.black,
+                                  height: 0.9,
+                                ),
+                              ),
+                              if (isFireDetected)
+                                GestureDetector(
+                                  onTap: () =>
+                                      _makePhoneCall(phoneNumber: '193'),
+                                  child: const Text(
+                                    "ligar para 193",
+                                    style: TextStyle(
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontFamily: AppStrings.appFontFamily,
+                                    ),
+                                  ),
+                                ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_flame/blocs/bluetooth/bluetooth_cubit.dart';
-import 'package:flutter_flame/blocs/bluetooth/bluetooth_state.dart';
-import 'package:flutter_flame/utils/app_strings.dart';
-import 'package:flutter_flame/utils/assets_manager.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_flame/screens/contact_screen.dart';
+import 'package:flutter_flame/screens/fire_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,101 +10,46 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  @override
-  void initState() {
-    super.initState();
-    final bluetoothCubit = context.read<BluetoothCubit>();
-    bluetoothCubit.requestPermissions().then((_) {
-      bluetoothCubit.connectToDevice(AppStrings.bluetoothConnectionAddress);
-    });
-  }
+  int _selectedIndex = 0;
 
-  Future<void> _makePhoneCall({required String phoneNumber}) async {
-    final Uri launchUri = Uri(
-      scheme: 'tel',
-      path: phoneNumber,
-    );
-    await launchUrl(launchUri, mode: LaunchMode.externalApplication);
+  static const List<Widget> _pages = <Widget>[FireScreen(), ContactScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Stack(
-          children: [
-            Image.asset(AssetsManager.imageCloud),
-            BlocBuilder<BluetoothCubit, BluetoothState>(
-              builder: (context, state) {
-                final bool isFireDetected = state.receivedData == "1";
-
-                if (state.isConnecting) {
-                  return Column(
-                    children: [
-                      const CircularProgressIndicator(),
-                      const SizedBox(height: 16),
-                      Text(state.statusMessage),
-                    ],
-                  );
-                }
-
-                return Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Image.asset(AssetsManager.imageTreeGif),
-                    Image.asset(
-                      isFireDetected
-                          ? AssetsManager.imageEyesFlame
-                          : AssetsManager.imageEyes,
-                    ),
-                    if (isFireDetected)
-                      Image.asset(AssetsManager.imageFlameGif),
-                    Column(
-                      children: [
-                        const SizedBox(
-                          height: 160,
-                        ),
-                        SizedBox(
-                          width: 260,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                isFireDetected
-                                    ? "fogo detectado"
-                                    : "tudo certo por aqui!",
-                                style: const TextStyle(
-                                  fontSize: 55,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: AppStrings.appFontFamily,
-                                  color: Colors.black,
-                                  height: 0.9,
-                                ),
-                              ),
-                              if (isFireDetected)
-                                GestureDetector(
-                                  onTap: () =>
-                                      _makePhoneCall(phoneNumber: '193'),
-                                  child: const Text(
-                                    "ligar para 193",
-                                    style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black,
-                                      fontFamily: AppStrings.appFontFamily,
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                );
-              },
-            ),
-          ],
+        child: _pages.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(80),
+          child: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: 30,
+                ),
+                label: 'home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.contact_page_rounded,
+                  size: 30,
+                ),
+                label: 'contatos',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
         ),
       ),
     );
