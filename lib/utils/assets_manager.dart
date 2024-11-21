6fc0c101288a243ef6c707b9c577358a86471262
jusylopes <jusylopes@outlook.com>
2024-11-21 02:08:48 -0300

    feat: add new animation and icon assets to AssetsManager for enhanced functionality

--- lib/utils/assets_manager.dart
+++ lib/utils/assets_manager.dart
@@ -3,12 +3,15 @@ class AssetsManager {
   static String fontFamilyBold = 'assets/fonts/Roboto-Bold.ttf';
 
   static String imagePath = 'assets/images';
-   static String imageLogo = '$imagePath/logo.png';
-  static String imageCloud = '$imagePath/cloud.gif';
-  static String imageEyesFlame = '$imagePath/eyes_flame.png';
-  static String imageEyes = '$imagePath/eyes.png';
+  static String imageLogo = '$imagePath/logo.png';
   static String imageFlameGif = '$imagePath/flame.gif';
 
-   static String animationPath = 'assets/animations';
-   static String animationTree = '$animationPath/bg_forest.riv';
+  static String animationPath = 'assets/animations';
+  static String animationForest = '$animationPath/forest.riv';
+  static String animationHappyFace = '$animationPath/happy_face.riv';
+  static String animationSadFace = '$animationPath/sad_face.riv';
+
+  static String iconPath = 'assets/icons';
+  static String iconHome = '$iconPath/icon_home.svg';
+  static String iconContact = '$iconPath/icon_contact.svg';
 }
