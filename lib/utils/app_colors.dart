75d63427cce7fa45179e461a51d5abb25982279e
jusylopes <jusylopes@outlook.om>
2025-01-07 19:54:08 -0300

    feat: remove deprecated screens and services to streamline codebase

--- lib/utils/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class AppColors {
-  static const Color backgroundColor = Color(0xFF90c1cc);
-  static const Color foregroundColor = Color(0xFF002222);
-  static const Color flameColor = Color(0xFFfd272e);
-  static const Color flameYellowColor = Color(0xFFfee688);
-}
